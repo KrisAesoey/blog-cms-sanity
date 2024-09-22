d233099b4154d794899b238d332af73814f1794e
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2024-09-22 14:51:15 +0200

    Add studio host

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'x2gstafu',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
+  studioHost: 'nedprioritert',
   autoUpdates: true,
 })
